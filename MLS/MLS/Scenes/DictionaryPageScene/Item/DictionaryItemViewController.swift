5fa2d54b2266f770187cf78ba4719128c925a9e6
dongglehada <ghddns34@gmail.com>
2024-01-28 16:37:40 +0900

    [INIT] dictionary viewcontroller init

---
+++ MLS/MLS/Scenes/DictionaryPageScene/Item/DictionaryItemViewController.swift
@@ -0,0 +1,8 @@
+//
+//  DictionaryItemViewController.swift
+//  MLS
+//
+//  Created by SeoJunYoung on 1/28/24.
+//
+
+import Foundation
---
+++ MLS/MLS/Scenes/DictionaryPageScene/Monster/DictionaryMonsterViewController.swift
@@ -0,0 +1,8 @@
+//
+//  DictionaryMonsterViewController.swift
+//  MLS
+//
+//  Created by SeoJunYoung on 1/28/24.
+//
+
+import Foundation

baac98851801f34cfa8635b44a941e2282cc9c8b
dongglehada <ghddns34@gmail.com>
2024-02-18 21:41:09 +0900

    [FEAT] DictLanding HeaderView UI

--- MLS/MLS/Scenes/DictionaryPageScene/DictLanding/DictLandingViewController.swift
+++ MLS/MLS/Scenes/DictionaryPageScene/DictLanding/DictLandingViewController.swift
@@ -1,8 +1,80 @@
 //
-//  DictLandingVIewController.swift
+//  DictLandingViewController.swift
 //  MLS
 //
 //  Created by SeoJunYoung on 2/18/24.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+
+class DictLandingViewController: BasicController {
+    // MARK: - Properties
+
+    var viewModel: DictLandingViewModel
+    
+    // MARK: - Components
+    
+    var headerView = DictLandingHeaderView()
+    
+    var firstSectionView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .blue
+        return view
+    }()
+    
+    var secondSectionView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .yellow
+        return view
+    }()
+
+    init(viewModel: DictLandingViewModel) {
+        self.viewModel = viewModel
+        super.init()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Life Cycle
+extension DictLandingViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUp()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.navigationBar.isHidden = true
+    }
+}
+
+// MARK: - SetUp
+private extension DictLandingViewController {
+    func setUp() {
+        setUpConstraints()
+    }
+    
+    func setUpConstraints() {
+        view.addSubview(headerView)
+        view.addSubview(firstSectionView)
+        view.addSubview(secondSectionView)
+        
+        headerView.snp.makeConstraints {
+            $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(56)
+        }
+        firstSectionView.snp.makeConstraints {
+            $0.top.equalTo(headerView.snp.bottom)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(204)
+        }
+        secondSectionView.snp.makeConstraints {
+            $0.top.equalTo(firstSectionView.snp.bottom)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+}
--- MLS/MLS/SharedViews/jobBadgeButton.swift
+++ MLS/MLS/SharedViews/jobBadgeButton.swift
@@ -5,4 +5,79 @@
 //  Created by SeoJunYoung on 2/18/24.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+
+class JobBadgeButton: UIButton {
+    // MARK: - Components
+    
+    private let stackView: UIStackView = {
+        let view = UIStackView()
+        view.spacing = Constants.spacings.xs_2
+        view.axis = .horizontal
+        return view
+    }()
+    
+    private let jobLabel: UILabel = {
+        let label = UILabel()
+        label.font = .customFont(fontSize: .caption_lg, fontType: .regular)
+        label.textColor = .black
+        return label
+    }()
+    
+    private let dotImage: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "Ellipse 1")
+        view.contentMode = .scaleAspectFit
+        return view
+    }()
+    
+    private let levelLabel: UILabel = {
+        let label = UILabel()
+        label.font = .customFont(fontSize: .caption_lg, fontType: .regular)
+        label.textColor = .black
+        return label
+    }()
+
+    init(job: String?, level: String?) {
+        super.init(frame: .zero)
+        self.backgroundColor = .yellow
+        self.jobLabel.text = job
+        self.levelLabel.text = level
+        setUp()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - SetUp
+private extension JobBadgeButton {
+    func setUp() {
+        setUpConstraints()
+        self.layer.cornerRadius = 12
+    }
+    
+    func setUpConstraints() {
+        
+        stackView.addArrangedSubview(jobLabel)
+        stackView.addArrangedSubview(dotImage)
+        stackView.addArrangedSubview(levelLabel)
+        self.addSubview(stackView)
+        
+        self.snp.makeConstraints {
+            $0.width.equalTo(58)
+            $0.height.equalTo(24)
+        }
+        stackView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(Constants.spacings.xs_2)
+            $0.leading.trailing.equalToSuperview().inset(Constants.spacings.sm)
+        }
+        dotImage.snp.makeConstraints {
+            $0.width.height.equalTo(Constants.spacings.xs_2)
+        }
+    }
+}

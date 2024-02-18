e773355c4368cc5de976e382588d401b3c67eecd
dongglehada <ghddns34@gmail.com>
2024-02-18 17:29:41 +0900

    [FEAT] Constants Spacing add

--- MLS/MLS/Helper/Constants.swift
+++ MLS/MLS/Helper/Constants.swift
@@ -7,14 +7,28 @@
 import UIKit
 
 enum Constants {
-    struct Default {
-        let vertical: CGFloat = 12
-        let horizontal: CGFloat = 16
-        let radius: CGFloat = 8
-        let blockHeight: CGFloat = UIScreen.main.bounds.height * 0.06
-    }
-
     static let defaults = Default()
+    static let spacings = Spacing()
     static let screenWidth = UIScreen.main.bounds.width
     static let screenHeight = UIScreen.main.bounds.height
+}
+
+struct Default {
+    let vertical: CGFloat = 12
+    let horizontal: CGFloat = 16
+    let radius: CGFloat = 8
+    let blockHeight: CGFloat = UIScreen.main.bounds.height * 0.06
+}
+
+struct Spacing {
+    let xs3: CGFloat = 1
+    let xs2: CGFloat = 2
+    let xs: CGFloat = 4
+    let sm: CGFloat = 8
+    let md: CGFloat = 16
+    let lg: CGFloat = 24
+    let xl: CGFloat = 32
+    let xl2: CGFloat = 40
+    let xl3: CGFloat = 56
+    let xl4: CGFloat = 80
 }

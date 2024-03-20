c55c9f4d70cd238601ea2988140c5c8e0717ecaa
p2glet <vviian.2@gmail.com>
2024-03-20 16:54:40 +0900

    [FEAT] : added annotation in DictQuestViewModel

--- MLS/MLS/Scenes/DictionaryPageScene/Quest/DictQuestViewModel.swift
+++ MLS/MLS/Scenes/DictionaryPageScene/Quest/DictQuestViewModel.swift
@@ -45,6 +45,8 @@ extension DictQuestViewModel {
         }
     }
     
+    /// 퀘스트의 반복회수와 defaultValues 속성을 DetailContent 타입의 배열로 변경
+    /// - Returns: 뷰에 뿌려질 DetailConetent 타입의 배열
     func fetchDefaultInfos() -> [DetailContent]? {
         guard let times = selectedQuest.value?.times else { return [] }
         var result = [DetailContent]()
@@ -53,6 +55,8 @@ extension DictQuestViewModel {
         return result
     }
     
+    /// 퀘스트의 완료조건을 DictDropContent타입의 배열인 self.completeTableContents에 저장
+    /// - Parameter completion: completeTableContents를 사용하는 뷰 reload 필요 시점
     func fetchCompleteInfos(completion: @escaping () -> Void) {
         guard let completions = selectedQuest.value?.toCompletion else { return }
         for completion in completions {
@@ -69,6 +73,8 @@ extension DictQuestViewModel {
         completion()
     }
     
+    /// 퀘스트 보상정보의 텍스트정보를 DetailContent타입의 배열로 변경
+    /// - Returns: 변경된 DetailConetnt 배열
     func fetchRewardTableContents() -> [DetailContent] {
         var result = [DetailContent]()
         selectedQuest.value.map {
@@ -89,6 +95,8 @@ extension DictQuestViewModel {
         return result
     }
     
+    /// 퀘스트 보상정보의 아이템정보를 DictDropContent타입의 배열인 self.rewardTableContents에 저장
+    /// - Parameter completion: rewardTableContents를 사용하는 뷰의 reload 필요 시점
     func fetchRewardInfos(completion: @escaping () -> Void) {
         guard let rewards = selectedQuest.value?.reward else { return }
         for reward in rewards {
